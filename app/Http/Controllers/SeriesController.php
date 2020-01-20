855b4c30c1e465702c1ac2ef217e548abd494e58
Mateus Teixeira <mateus.teixeira@trackerup.com.br>
2020-01-20 10:27:27 -0300

    Commit inicial

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller {
+    public function index(){
+        $series = [
+            'Teste 1',
+            'Teste 2',
+            'Teste 3'
+        ];
+
+        return view('series.index', compact('series'));
+    }
+
+    public function create(){
+        return view('series.create');
+    }
+}
---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('cabecalho')
+    Adicionar série
+@endsection
+
+@section('conteudo')
+    <form method="POST">
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input id="nome" type="text" class="form-control" name="nome"/>
+        </div>
+        <button class="btn btn-primary">Adicionar</button>
+    </form>
+@endsection
