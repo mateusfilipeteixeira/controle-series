ba84b428a6b368439e50f2405743d50badfc9bbc
Mateus Teixeira <mateus.teixeira@trackerup.com.br>
2020-01-22 09:27:36 -0300

    Flash message

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,9 +6,13 @@ use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller {
-    public function index(){
+    public function index(Request $request){
         $series = Serie::query()->orderBy('nome')->get();
-        return view('series.index', compact('series'));
+
+        $mensagem = $request->session()->get('mensagem');
+
+
+        return view('series.index', compact('series', 'mensagem'));
     }
 
     public function create(){
@@ -17,6 +21,11 @@ class SeriesController extends Controller {
 
     public function store(Request $request){
         $serie = Serie::create($request->all());
+        $request->session()
+            ->flash(
+                'mensagem',
+                "Série {$serie->id} criada com sucesso: {$serie->nome}"
+            );
         return redirect('/series');
     }
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -5,6 +5,12 @@
 @endsection
 
 @section('conteudo')
+    @if(!empty($mensagem))
+        <div class="alert alert-success">
+            {{ $mensagem }}
+        </div>
+    @endif
+
     <a href="/series/create" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
